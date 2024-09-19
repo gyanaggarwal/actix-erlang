1a31c213c6df620baf98a9e74842f4ed7b011ffd
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2024-09-18 19:27:06 -0500

    added logic to populate peer_addr

---
+++ src/actix_erl.rs
@@ -0,0 +1,104 @@
+use actix::prelude::*;
+
+use std::collections::HashMap;
+
+type NodeType = u16;
+type RecValue = Addr<CrdtActor>;
+type NRMap = HashMap<NodeType, RecValue>;
+
+#[derive(Message, Clone)]
+#[rtype(result = "()")]
+pub struct UserMsg {
+    pub msg_value: u64
+}
+
+impl UserMsg {
+    pub fn new(msg_value: u64) -> Self {
+        Self{msg_value}
+    }
+}
+
+#[derive(Message, Clone)]
+#[rtype(result = "()")]
+pub struct PeerMsg {
+    pub peer_value: u64
+}
+
+impl PeerMsg {
+    pub fn new(peer_value: u64) -> Self {
+        Self{peer_value}
+    }
+}
+
+#[derive(Message, Clone)]
+#[rtype(result = "()")]
+pub struct Receipients {
+    pub rcpts: NRMap
+}
+
+impl Receipients {
+    pub fn new(rcpts: NRMap) -> Self {
+        Self{rcpts}
+    }
+}
+#[derive(Debug, Clone)]
+pub struct CrdtActor {
+    pub node: NodeType,
+    pub crdt_value: u64,
+    pub receipients: Option<NRMap>
+}
+
+impl Actor for CrdtActor {
+    type Context = Context<Self>;
+}
+
+impl CrdtActor {
+    pub fn new(node: NodeType) -> Self {
+        Self {node, crdt_value: 0, receipients: None}
+    }
+}
+
+impl Handler<PeerMsg> for CrdtActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: PeerMsg, _ctx: &mut Self::Context) -> Self::Result {
+        self.crdt_value += msg.peer_value;
+
+        println!("self:handle:peer_msg {:?}", self);
+    }
+}
+
+impl Handler<UserMsg> for CrdtActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: UserMsg, _ctx: &mut Self::Context) -> Self::Result {
+        self.crdt_value += msg.msg_value;
+        let peer_msg = PeerMsg::new(msg.msg_value); 
+        if let Some(rec) = &self.receipients {
+            for (_, addr) in rec {
+                addr.do_send(peer_msg.clone());
+            }
+        }
+
+        println!("self:handle:user_msg {:?}", self)
+    }
+}
+
+impl Handler<Receipients> for CrdtActor {
+    type Result = ();
+
+    fn handle(&mut self, msg: Receipients, _ctx: &mut Self::Context) -> Self::Result {
+        let fmap: NRMap = msg.rcpts.iter()
+            .filter_map(|(rnode, value)|{
+                if *rnode != self.node {
+                    Some((*rnode, value.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        self.receipients = Some(fmap);
+
+        println!("self:handle:rcpts {:?}", self);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix::prelude::*;
+use std::collections::HashMap;
+
+use actix_erlang::actix_erl::{CrdtActor, Receipients};
+
+#[actix::main]
+async fn main() {
+    let _ = test_crdt().await;
+}
+
+
+async fn test_crdt() {
+    let mut rmap: HashMap<u16, Addr<CrdtActor>>= HashMap::new();
+
+    let crdt0 = CrdtActor::new(0);
+    let crdt1 = CrdtActor::new(1);
+    let crdt2 = CrdtActor::new(2);
+
+    let addr0 = crdt0.start();
+    let addr1 = crdt1.start();
+    let addr2 = crdt2.start();
+    rmap.insert(0, addr0.clone());
+    rmap.insert(1, addr1.clone());
+    rmap.insert(2, addr2.clone());
+
+    let rcpts = Receipients::new(rmap);
+
+    addr0.send(rcpts.clone()).await.unwrap();
+    addr1.send(rcpts.clone()).await.unwrap();
+    addr2.send(rcpts.clone()).await.unwrap();
 }
